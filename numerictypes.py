5c181ba19ba200b1b08e662471265072e1d2fc20
Fayk Chaudhry <faykc123@gmail.com>
2020-03-30 23:00:02 -0400

    Added several numeric types

--- numerictypes.py
+++ numerictypes.py
@@ -17,5 +17,19 @@ print(type(z)) # Yields (float)
 z = 30
 z += 0.1
 print(z)
-print(type(z)) # Yields (float), converted automatically, could be dangerous...
+print(type(z)) # Yields (float), converted automatically, (could be dangerous)..
+
+d = 2 + 5j  # This would not be allowed in C HOLY!
+print(d)
+print(type(d))  # Strange complex types
+
+e = 0B1010  # Binary Value
+print(e)
+print(type(e)) # Prints <int>
+
+f = 0XFF    # Hexadecimal
+print(f)
+
+o = 0o74 #Octodecimal value
+print(o)
 
