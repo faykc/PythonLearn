e1e54d3810c6cf95f48bebf1ce6472a436298261
Fayk Chaudhry <faykc123@gmail.com>
2020-03-30 22:09:53 -0400

    Added comments

--- introduction.py
+++ introduction.py
@@ -1,1 +1,6 @@
+#Single Line Comment
+""" This is a multi line comment
+    That we will be adding more lines too
+    This is an interesting multiline comment syntax"""
+    
 print ("here we go again")
