59ccae138304111c8e760da19bd7048759321210
Fayk Chaudhry <faykc123@gmail.com>
2020-03-31 23:49:16 -0400

    More List functions

--- ListType.py
+++ ListType.py
@@ -1,2 +1,21 @@
 lst=[] #This is an empty list! 
 print(lst) 
+lst2 = ["30", 28, [1,2,3,4,"4"],10.3] #Lists in lists in lists
+print(lst2)
+print(lst2[0])
+print(lst2[2])
+print(lst2[0:2])
+print(lst2[0::2])
+lst2.append(20)
+print(lst2)
+lst2.remove(28)
+print(lst2)
+del(lst2[0])
+print(lst2)
+#lst2.clear()
+#print(lst2)
+
+print(lst2)
+lst2.insert(0,"Hello")
+print(lst2)
+
