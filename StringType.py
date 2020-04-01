7788415f3596f40e3e030cde889e028aa187bd1c
Fayk Chaudhry <faykc123@gmail.com>
2020-03-31 22:31:02 -0400

    Testing with String Functions

--- StringType.py
+++ StringType.py
@@ -4,7 +4,7 @@ print(s)
 print(a)
 print(type(s))
 
-multiline = """I gues this is a
+multiline = """I guess this is a
                 multiline comment maybe?"""
 print(multiline)
 print(s[0]) #First character of s hahah similar to C
@@ -17,5 +17,20 @@ print(len(s))
 for x in s:
     print(x) # Print every letter in s
 
+print(s[0:5]) # Prints the 0th to the 5th index in string interest...
+print(s[0:])
+print(s[:8])
+x : str = "abcdefghijklmnopqrstuvwxyz"
+print(x[0::2]) # Prints every other character
+print(x[::2]) # Prints every other character
 
+print(x[-4:-1]) #Interesting, -1 represents last element and it goes backwards
 
+print(x[0:9:2])
+print(x[15::-1]) #Step backwards interesting
+print(x[::-1]) #Step totally backwards
+
+print(x.replace("a","A"))
+
+print(x)
+print(x.strip())
