658910ee7c41b67380ea92399b9c679a02e03e7c
Fayk Chaudhry <faykc123@gmail.com>
2020-03-31 22:11:30 -0400

    Added more stuff

---
+++ StringType.py
@@ -0,0 +1,21 @@
+s = "this is a string"
+a : str = "Specifically defined string"
+print(s)
+print(a)
+print(type(s))
+
+multiline = """I gues this is a
+                multiline comment maybe?"""
+print(multiline)
+print(s[0]) #First character of s hahah similar to C
+print(s[1])
+print(s*2) # This is so odd yet interesting!
+print(s + " " + s)
+
+print(len(s))
+
+for x in s:
+    print(x) # Print every letter in s
+
+
+
