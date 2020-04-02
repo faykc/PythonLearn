8a75f390962fefc5e25ecb6417415a00f02d179b
Fayk Chaudhry <faykc123@gmail.com>
2020-04-01 23:35:46 -0400

    Range type and for loops

---
+++ rangeType.py
@@ -0,0 +1,8 @@
+r = range(5)
+x = range(0,10,2)
+
+for i in r:
+    print(i)
+
+for i in x:
+    print(i)
--- settype.py
+++ settype.py
@@ -2,3 +2,10 @@ s = {10,20,"string",10,10,10}
 print(type(s))
 s.update([99])
 print(s)
+s.remove(10)
+print(s)
+
+f=frozenset({10,20,30})
+x=frozenset([10,23,30])
+print(f)
+print(x)
