0bf40c83eb405d2ede942ba11230a1b22323bb47
Fayk Chaudhry <faykc123@gmail.com>
2020-04-01 23:46:57 -0400

    Added support for Dictionaries

---
+++ Dictionaries.py
@@ -0,0 +1,18 @@
+d = {
+    "FC": "cool",
+    "LOL": "Somewhat cool"
+}
+
+print(d)
+print(d.values())
+print(d.keys())
+x = d.keys()
+
+for i in x:
+    print(i)
+
+print(d["FC"])
+
+del d["FC"]
+
+print(d)
