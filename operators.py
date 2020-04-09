c1f5c69015eda5dbf6bc3f490c719cb07dc8ab2a
Fayk Chaudhry <faykc123@gmail.com>
2020-04-08 23:22:04 -0400

    Basic operatios

---
+++ operators.py
@@ -0,0 +1,11 @@
+
+
+a,b=5
+
+print('Addition', a+b)
+print('Subtraction', a-d)
+print('Multiplication', a*b)
+print('Exponentails!!!', a**b)
+print('Division', a/b)
+print('Division with rounding down', a//d)
+
