9406e5160869244f6d4e7d7feb689ccaf6f30252
Fayk Chaudhry <faykc123@gmail.com>
2020-04-08 23:27:22 -0400

    Added some comments

--- operators.py
+++ operators.py
@@ -1,11 +1,12 @@
 a,b=10,5
 
+# This is basic arithmetic operations of python
 print('Addition', a+b)
 print('Subtraction', a-b)
 print('Multiplication', a*b)
-print('Exponentails!!!', a**b)
+print('Exponentails!!!', a**b) # This is exponential of a to the exponent b
 print('Division', a/b)
-print('Division with rounding down', a//b)
+print('Division with rounding down', a//b) # This is floor division, rounds down to 0
 
 
 
