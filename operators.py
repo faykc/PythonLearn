9af9e2a2e69b95182fb28353905e624a54429d19
Fayk Chaudhry <faykc123@gmail.com>
2020-04-08 23:34:06 -0400

    Boolean operators addition

--- operators.py
+++ operators.py
@@ -8,6 +8,21 @@ print('Exponentails!!!', a**b) # This is exponential of a to the exponent b
 print('Division', a/b)
 print('Division with rounding down', a//b) # This is floor division, rounds down to 0
 
+a+=b
+a=b=c=10 
+print(a,b,c)
+
+a*=b
+print(a)
+
+#You can also do comparison operators as well too
+x,y=77,88
+print(x==y)
+print(x!=y)
+print(x>=y)
+print(x<=y)
+
+
 
 
 
