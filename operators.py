6d6677c32033e12e6202b3d0518d2aebbd4b631a
Fayk Chaudhry <faykc123@gmail.com>
2020-04-08 23:24:24 -0400

    Fixed some minor glitches

--- operators.py
+++ operators.py
@@ -1,11 +1,12 @@
-
-
-a,b=5
+a,b=10,5
 
 print('Addition', a+b)
-print('Subtraction', a-d)
+print('Subtraction', a-b)
 print('Multiplication', a*b)
 print('Exponentails!!!', a**b)
 print('Division', a/b)
-print('Division with rounding down', a//d)
+print('Division with rounding down', a//b)
+
+
+
 
