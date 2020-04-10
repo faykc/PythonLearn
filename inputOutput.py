3fd90d61eef6eef4fad7fd26ae4cf2531c9bb228
Fayk Chaudhry <faykc123@gmail.com>
2020-04-10 00:03:21 -0400

    Last operator comparsion with not and new file:

---
+++ inputOutput.py
@@ -0,0 +1,2 @@
+#On this file we will be learning how to deal with user input
+
--- operators.py
+++ operators.py
@@ -26,3 +26,4 @@ x,y=20,30
 print(x==20 and y==30)
 print(x==21 or y==30)
 print(x==255555 and y==30)
+print(not(x==21 and  y==30))
