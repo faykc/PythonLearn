eb009026818d448c876d0f37b7f330daa97497d0
Fayk Chaudhry <faykc123@gmail.com>
2020-04-10 00:24:22 -0400

    Getting user input:

--- inputOutput.py
+++ inputOutput.py
@@ -5,4 +5,11 @@ print("nothing"*3)
 a,b=10,20
 print(a,b,sep='')
 
+name="Name"
+print("My name is %s"%(name))
+
+print("My name is {}".format(name))
+
+s = input()
+print("The input given was %s"%(s))
 
